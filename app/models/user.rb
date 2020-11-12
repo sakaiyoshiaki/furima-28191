c94bc7eeafb9562a8a9add3d9b63efbe52b785af
sakaiyoshiaki <yo4sio11.abc@gmail.com>
2020-11-12 22:49:04 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable,:validatable
+         :recoverable, :rememberable, :validatable
 
   with_options presence: true do
     validates :nickname
     validates :birthday
-    validates :email, uniqueness:true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
     validates :encrypted_password, length: { minimum: 6 }
 
     with_options format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ } do
--- config/application.rb
+++ config/application.rb
@@ -18,4 +18,7 @@ module Furima28191
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
+  class Application < Rails::Application
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+  end  #field_with_errorsの自動生成によるレイアウト崩れを防ぐ
 end
