3b4b626d0867c420b65f365e1097255d679fb58d
sakaiyoshiaki <yo4sio11.abc@gmail.com>
2020-11-15 16:39:19 +0900

    デプロイ

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,11 @@ module Furima28191
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.assets.initialize_on_precompile = false #デプロイ関連
-    config.autoload_paths << Rails.root.join("lib") #デプロイ関連：lib以下の読み込み
+    config.autoload_paths << Rails.root.join("bin") #デプロイ関連：bin以下の読み込み
+    # config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    # config.eager_load_paths += Dir["#{config.root}/lib/**/"]
+    # config.autoload_paths += Dir["#{config.root}/bin/**/"]
+    # config.eager_load_paths += Dir["#{config.root}/bin/**/"]
   end
   class Application < Rails::Application
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
