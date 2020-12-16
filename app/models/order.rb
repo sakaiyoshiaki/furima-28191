623e5cce412bd35419ca505e277c4f8432123e0d
sakaiyoshiaki <yo4sio11.abc@gmail.com>
2020-12-17 00:37:00 +0900

    バリデーションの設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,15 @@
 class Order < ApplicationRecord
   belongs_to :item_purchase
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
+  with_options presence: true do
+    validates :postal_code,format:{with:/\A\d{3}[_]\d{4}\z/}
+    validates :prefecture_id
+    validates :city
+    validates :house_number
+    validates :building_name
+    validates :tel,length: {minimum: 11}
+  end
 end
