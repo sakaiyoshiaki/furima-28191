0c7c375cb6d1763b2a813f9badc73a1f8db6fda3
sakaiyoshiaki <yo4sio11.abc@gmail.com>
2020-12-21 11:28:50 +0900

    売却済みの商品は「sold out」を表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
+  before_action :authenticate_user!, only: [:index] # 未ログインユーザーが購入ページに遷移しようとすると、ログインページに遷移する
   before_action :correct_user, only: [:index]
   before_action :set_item, only: [:index, :create]
 
