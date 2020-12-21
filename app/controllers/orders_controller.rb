c8a6d6992f65709d59eedf2653c09e0bfb98bdc9
sakaiyoshiaki <yo4sio11.abc@gmail.com>
2020-12-21 10:54:14 +0900

    出品者がURLを直接入力して購入ページに遷移しようとすると、トップページに遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  before_action :correct_user, only: [:index]
   before_action :set_item, only: [:index, :create]
 
   def index
@@ -29,15 +30,19 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     Payjp.api_key = ENV['PAYJP_SECRET_KEY'] # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
-      # amount: order_params[:price],
       amount: @item.price, # 商品の値段
       card: order_params[:token], # カードトークン
-      # card: :token,
-      currency: 'jpy'                 # 通貨の種類(日本円)
+      currency: 'jpy' # 通貨の種類(日本円)
     )
   end
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def correct_user
+    # 出品者がURLを直接入力して購入ページに遷移しようとすると、トップページに遷移する
+    @item = Item.find(params[:item_id])
+    redirect_to root_url if @item.user_id == current_user.id
   end
 end
--- app/models/order_itempurchase.rb
+++ app/models/order_itempurchase.rb
@@ -2,7 +2,6 @@ class OrderItempurchase
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :tel, :user_id, :item_id, :token
 
-
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'にハイフンを含めて下さい' }
     validates :city
