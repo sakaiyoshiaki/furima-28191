14f2749238296688e86f260fefbc1d2742acf527
sakaiyoshiaki <yo4sio11.abc@gmail.com>
2020-12-26 11:19:00 +0900

    バリデーションの修正(パスワード)

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,10 @@ class User < ApplicationRecord
     validates :nickname
     validates :birthday
     validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-    validates :encrypted_password, length: { minimum: 6 }
+    validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+    validates :password_confirmation
 
-    with_options format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ } do
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
       validates :family_name
       validates :personal_name
     end
