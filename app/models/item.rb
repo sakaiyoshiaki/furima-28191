5abc8c0dce4f9cd567e5534820d81c75bc9d78a1
sakaiyoshiaki <yo4sio11.abc@gmail.com>
2020-12-26 16:36:46 +0900

    バリデーションの追加(image)

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,6 +11,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
+    validates :image, presence: { message: 'を選択してください' }
     validates :title, length: { maximum: 40 }
     validates :description, length: { maximum: 1000 }
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
